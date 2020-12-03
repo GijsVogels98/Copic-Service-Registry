d0bc92278e292f45f3cfe91cb91a10d3ee8b551d
Gijs <gijsvogels@hotmail.nl>
2020-12-03 12:48:40 +0100

    first commit

---
+++ src/test/java/com/copic/service/registery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copic.service.registery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
